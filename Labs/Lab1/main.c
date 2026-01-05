6b6e803b82b4e193622e72824f816ebaa24ba7af
Luyu Wu <luyuwuvk@gmail.com>
2026-01-05 11:25:55 -0500

    init

---
+++ Labs/Lab1/main.c
@@ -0,0 +1,8 @@
+void main {
+	int a = 5;
+	
+}
+
+void set (int integer){
+	integer = 10;
+}
---
+++ Projects/Project1/autocomplete.c
@@ -0,0 +1,15 @@
+typedef struct term{
+	char term[200]; // assume terms are not longer than 200
+	double weight;
+} term;
+
+int lowest_match(term *terms, int nterms, char *substr) {
+	
+} index;
+
+void main {
+
+}
+
+
+
